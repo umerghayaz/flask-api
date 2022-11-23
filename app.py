f4ed9ba61e209143736bbfa79c52ca2c31df4ba7
Umer <UMERSHAHID667@gmail.com>
2022-11-23 12:09:20 +0500

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://japiosmuawoeif:5fb7865a30ccd9848356a018582cee7186472ddafe35b23aa61cbe6fa969a357@ec2-54-86-106-48.compute-1.amazonaws.com:5432/d4e37uperu0jod'
 db = SQLAlchemy(app)
 from flask_sqlalchemy import SQLAlchemy
+CORS(app)
 
 # Create our database model
 class User(db.Model):
@@ -25,6 +27,9 @@ def index():
     return render_template('index.html')
 
 # Save e-mail to database and send to success page
+
+
+@cross_origin()
 @app.route('/prereg', methods=['POST'])
 def prereg():
     pet_data = request.get_json()
